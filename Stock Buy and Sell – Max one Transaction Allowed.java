dfef212c25596bd9223af0b2fc2b96eddd73aeb7
Puskar Nath <puskarnath110@gmail.com>
2026-01-19 19:52:36 +0530

    19.01.2026

---
+++ Stock Buy and Sell – Max one Transaction Allowed.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+class Solution {
+    public int maximumProfit(int prices[]) {
+         
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+ }
