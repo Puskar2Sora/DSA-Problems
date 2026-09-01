e0040ea56f045ad37934d0d1a4cf93009ecdd97f
Puskar Nath <puskarnath110@gmail.com>
2026-09-01 21:45:23 +0530

    String Class maintained

--- LeetCode 75/String_Compression.java
+++ LeetCode 75/String_Compression.java
@@ -1,16 +1,15 @@
 class String_Compression {
     public int compress(char[] chars) {
         int write = 0, count = 1;
-
         for (int i = 0; i < chars.length; i++) {
             if (i + 1 < chars.length && chars[i] == chars[i + 1])
                 count++;
             else {
                 chars[write++] = chars[i];
-                if (count > 1) {
-                    for (char c : Integer.toString(count).toCharArray()) {
+                if (count > 1)
+                    {
+                    for (char c : Integer.toString(count).toCharArray())
                         chars[write++] = c;
-                    }
                 }
                 count = 1;
             }
